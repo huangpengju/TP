04da77569ea1afff098ec38dc29a1ac62dc37e34
huangpengju <15713716933@163.com>
2016-12-12 11:29:34 +0800

    毕业设计

---
+++ Project/index.php
@@ -0,0 +1,9 @@
+<?php 
+	//调试
+	define("APP_DEBUG",true);//用于显示错误
+	//创建应用目录  注意Myweb/后面的/
+	define("APP_PATH","./Myweb/");
+	//
+	//导入ThinkPHP ThinkPHP.php文件
+	require('./ThinkPHP/ThinkPhP.php');
+ ?>
